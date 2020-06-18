a93a7c7fb38324d6e5b4901f73aef420775a4a3f
Daniel <triotk@gmail.com>
2020-06-18 16:33:44 +0900

    dd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res, next) => {
-  res.send("HELLO HARAM");
+  res.send("HELLO HARAM!");
 });
 
 app.listen(PORT, () => console.log("THE SERVER IS RUNNING ON PORT" + PORT));
